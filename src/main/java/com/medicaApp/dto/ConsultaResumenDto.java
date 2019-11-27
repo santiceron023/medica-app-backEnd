a6ec7a56de8003099aa5415384259fd0ede8583d
Santiago Felipe Ceron Araujo <santiceron023@gmail.com>
2019-11-27 07:24:12 -0500

    missing files

---
+++ src/main/java/com/medicaApp/dto/ConsultaResumenDto.java
@@ -0,0 +1,22 @@
+package com.medicaApp.dto;
+
+public class ConsultaResumenDto {
+	
+	String fecha;
+	String cantidad;
+	
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public String getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(String cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+
+}
